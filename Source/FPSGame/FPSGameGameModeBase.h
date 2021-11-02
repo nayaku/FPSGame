84b67c4d7db3b183beb6652e979a49c6ae21d3c3
nayaku <736248591@qq.com>
2021-11-02 14:58:57 +0800

    Init2

---
+++ Source/FPSGame/FPSGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSGAME_API AFPSGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
--- Source/MyFPS/MyFPS.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MyFPS.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyFPS, "MyFPS" );
